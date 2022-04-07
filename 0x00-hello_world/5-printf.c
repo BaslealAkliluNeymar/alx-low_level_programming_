74085330a4cea570458112dcfd0afb547c48846b
BaslealAkliluNeymer <baslealaklilu@gmail.com>
2022-04-07 09:24:49 -0700

    ddd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
-* Description: main -Prints out a string to stdout
+* Description: main -Prints out a string to stdout 
 * Return:0 if success
 */
 
@@ -9,7 +9,7 @@ int main(void)
 
 {
 
-printf("with proper grammar,but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
