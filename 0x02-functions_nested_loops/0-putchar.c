d7dccdb9ae65cb28b41e82868cacf5a4d08c6fd7
BaslealAkliluNeymer <baslealaklilu@gmail.com>
2022-04-12 12:10:19 -0700

    eer

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,15 @@
 */
 int main(void)
 {
-char me[] = "_putchar";
-int c;
-for (c = 0; c < 7; c++)
-{
-_putchar(me[c]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+	   
 return (0);
 }
