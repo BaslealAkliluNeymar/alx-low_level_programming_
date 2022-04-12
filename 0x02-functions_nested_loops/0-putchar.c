c90341f654c7b51aebe651d601869b45c6e425ed
BaslealAkliluNeymer <baslealaklilu@gmail.com>
2022-04-12 03:23:46 -0700

    ee

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-* main - Entry point 
-* @void:no argument
-* REturn: 0
+* main -Entry point
 *
+* Return: Alwasys 0 (success/correct)
+*/
 int main(void)
 {
 char me[] = "_putchar";
 int c;
-for ( c = 0; c < 8; c++)
+for (c = 0; c < 8; c++)
 {
 _putchar(me[c]);
+}
 return (0);
 }
