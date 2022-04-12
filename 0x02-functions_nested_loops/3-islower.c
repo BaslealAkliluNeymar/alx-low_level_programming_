458b610a8b84b1b0138ecf69ea843a25c240e93a
BaslealAkliluNeymer <baslealaklilu@gmail.com>
2022-04-12 06:06:26 -0700

    rr

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* islower - picks the lower and sends
+
+
+* Description - finds the lowest and returns 0 or 1
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (0);
+}
+else
+{
+return (1);
+}
+D
